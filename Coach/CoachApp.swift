7577e3acbaa6eabd89c5dd7dd6481e0fec8d55f9
sudharkj <sudha.rkj@gmail.com>
2022-02-17 21:21:07 -0800

    Initial Commit

---
+++ Coach/CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachApp.swift
+//  Coach
+//
+//  Created by Sudha Ravi Kumar Javvadi on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
